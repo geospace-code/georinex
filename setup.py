42748a69f15a0875621ed2263d474026b37e4065
michael <scienceopen@users.noreply.github.com>
2016-05-13 01:05:51 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import subprocess
+
+try:
+    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+except Exception as e:
+    print('you will need to install packages in requirements.txt  {}'.format(e))
 
 with open('README.rst','r') as f:
 	long_description = f.read()
