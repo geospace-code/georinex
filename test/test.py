d65d2b4aec0958ffaa999e6a5b78a5c2e087b277
michael <scienceopen@noreply.github.com>
2016-06-03 01:16:41 -0400

    functionalize self test

--- test/test.py
+++ test/test.py
@@ -4,20 +4,22 @@ Self-test file, registration case
 for OBS RINEX reader
 """
 from pandas.io.pytables import read_hdf
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose,run_module_suite
 #
 from pyrinex.readRinexObs import rinexobs
 from pyrinex.readRinexNav import readRinexNav
 
-#%% do registration case
-truth = read_hdf('test/demo.h5',key='OBS')
+def test_obs():
+    truth = read_hdf('test/demo.h5',key='OBS')
+    blocks = rinexobs('test/demo.10o')
+    
+    assert_allclose(blocks,truth)
+    
+def test_nav():
+    truthnav = read_hdf('test/demo.h5',key='NAV')
+    testnav = readRinexNav('test/demo.10n')
+    
+    assert_allclose(testnav,truthnav)
 
-blocks = rinexobs('test/demo.10o',False,None)
-
-assert_allclose(blocks,truth)
-
-#%%
-truthnav = read_hdf('test/demo.h5',key='NAV')
-testnav = readRinexNav('test/demo.10n',False)
-
-assert_allclose(testnav,truthnav)
+if __name__ == '__main__':
+    run_module_suite()
