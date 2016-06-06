7d1a8416541cb0e7955d88aea0ecdf6fbc5d971b
scivision <scivision@users.noreply.github.com>
2016-06-05 23:58:36 -0400

    pathlib

--- test/test.py
+++ test/test.py
@@ -3,23 +3,28 @@
 Self-test file, registration case
 for OBS RINEX reader
 """
+from pyrinex import Path
 from pandas.io.pytables import read_hdf
 from numpy.testing import assert_allclose,run_module_suite
 #
 from pyrinex.readRinexObs import rinexobs
 from pyrinex.readRinexNav import readRinexNav
 
+rdir=Path(__file__).parents[1]
+
 def test_obs():
-    truth = read_hdf('test/demo.h5',key='OBS')
-    blocks = rinexobs('test/demo.10o')
+    truth = read_hdf(str(rdir/'test/demo.h5'),key='OBS')
+    blocks = rinexobs(str(rdir/'test/demo.10o'))
     
     assert_allclose(blocks,truth)
     
 def test_nav():
-    truthnav = read_hdf('test/demo.h5',key='NAV')
-    testnav = readRinexNav('test/demo.10n')
+    truthnav = read_hdf(str(rdir/'test/demo.h5'),key='NAV')
+    testnav = readRinexNav(str(rdir/'test/demo.10n'))
     
     assert_allclose(testnav,truthnav)
 
 if __name__ == '__main__':
+    test_obs()
+    test_nav()
     run_module_suite()
