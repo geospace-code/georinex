db10f11a3484ef2259ee080ed8c671552a59679b
scienceopen <scienceopen@users.noreply.github.com>
2016-06-26 14:32:21 -0500

    cleanup

--- pyrinex/__init__.py
+++ pyrinex/__init__.py
@@ -4,11 +4,4 @@ if PY2:
     from pathlib2 import Path
 else:
     from pathlib import Path
-#%% apply future to each module
-flist = Path(__file__).parent.glob('*.py')
-flist = [f for f in flist if f!='__init__.py']
-__all__=flist
-
-for f in flist:
-    from . import f #thereby applying __future__
 
