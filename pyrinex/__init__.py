4aff743f60067646349c910907af517b47e70d5d
scienceopen <scienceopen@users.noreply.github.com>
2016-07-11 20:09:33 -0400

    cleanup

--- pyrinex/__init__.py
+++ pyrinex/__init__.py
@@ -1,6 +1,6 @@
 try:
     from pathlib import Path
-    Path().expanduser() #fails python<3.5
+    Path().expanduser()
 except (ImportError,AttributeError):
     from pathlib2 import Path
 
--- setup.py
+++ setup.py
@@ -3,17 +3,14 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
-except (Exception,KeyboardInterrupt) as e:
+    subprocess.call(['conda','install','--file','requirements.txt'])
+except Exception as e:
     pass
 
-with open('README.rst','r') as f:
-	long_description = f.read()
 #%% install
 setup(name='pyrinex',
       packages=['pyrinex'],
 	  description='Python RINEX reader that is very fast',
-	  long_description=long_description,
       install_requires=['pathlib2'],
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/pyrinex',
