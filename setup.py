e7068b8821a5f3ad9c448eae3c0150d0916e979c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-20 02:45:18 -0500

    template

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 req = ['nose','numpy','numpy','xarray','netcdf4','pathlib2']
 # %%
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(name='pyrinex',
-      packages=['pyrinex'],
+      packages=find_packages(),
 	  description='Python RINEX reader that is very fast',
 	  author='Michael Hirsch, Ph.D.',
       version='1.1.0',
