c2d633cac7a0822cf62de17d24282404ed04b492
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-06-08 12:32:17 -0400

    time slice demo

--- PlotExample.py
+++ PlotExample.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 """
 PyRINEX plotting example
+
+includes how to index by satellite, measurement type and time
 """
 from pathlib import Path
+import numpy as np
+from datetime import datetime
 import pyrinex as pr
 import matplotlib.dates as md
 from matplotlib.pyplot import figure, show
@@ -11,6 +15,13 @@ fn = Path('~/data/GEOP107Q.18o')
 
 obs, nav = pr.readrinex(fn, use='G')
 
+# %% optional time indexing demo
+tlim = (datetime(2018, 4, 17, 16, 25),
+        datetime(2018, 4, 17, 16, 30))
+
+# boolean indexing  -- set "i=slice(None)" to disable time indexing.
+i = (obs.time >= np.datetime64(tlim[0])) & (obs.time <= np.datetime64(tlim[1]))
+# i = slice(None)
 # %% plot
 SV = 'G14'
 titles = ['Psedoranges of GPS and Glonass', 'Carrier Phase', 'Doppler', 'Signal Strength']
@@ -24,7 +35,7 @@ for v, title, ylabel, ax in zip(('C1C', 'L1C', 'D1C', 'S1C'), titles, ylabels, a
     if v not in obs:
         continue
 
-    Satobs = obs[v].sel(sv=SV).dropna(dim='time', how='all')
+    Satobs = obs[v][i].sel(sv=SV).dropna(dim='time', how='all')
 
     Satobs.plot(ax=ax)
 
