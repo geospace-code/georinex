81f97ec3010d203c5e6ef2776997cec8f875849c
aldebaran1 <sebastijan.mrak@gmail.com>
2018-08-25 17:39:17 -0400

    Utils function to convert an OBS folder/file into NC dataset

---
+++ utils/rnx2nc.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 13:25:38 2017
+
+@author: Sebastijan Mrak <smrak@gmail.com>
+"""
+import os
+import glob
+import platform
+from time import sleep
+import georinex as gr
+
+def _convert(file, odir):
+    print ('Converting file: ', file)
+    try:
+        gr.load(file,ofn=odir)
+    except Exception as e:
+        print (e)
+    sleep(0.1)
+    
+
+def convertObs2HDF(folder=None, sufix=None, odir=None, override=False):
+    """
+    This script converts RINEX 2.11 observation files in a given directory into
+    a hdf5 organized data structure, utilizing pyRINEX script. Find the script
+    in the main directory.
+    """
+    if sufix is None:
+        wlist = ['*.**o', '*.**O']
+    else:
+        wlstr = sufix
+    if odir is None:
+        odir = folder
+    for wlstr in wlist:
+        filestr = os.path.join(folder,wlstr)
+        flist = sorted(glob.glob(filestr))
+        for file in flist:
+            head, tail = os.path.split(file)
+            rx = tail[0:8]
+            if platform.system() == 'Linux':
+                newfn = odir + '/' + rx + '.nc'
+            elif platform.system() == 'Windows':
+                newfn = odir + '\\' + rx + '.nc'
+            if not override:
+                if not os.path.exists(newfn):
+                    _convert(file, odir)
+            else:
+                _convert(file, odir)
+                
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('folder',type=str)
+    p.add_argument('-odir', '--odir', help='Destination folder, if None-> the same as input folder', default=None)
+    p.add_argument('-f', '--force', help="Force override, if the NC file already exist", default=False, type=bool)
+    p.add_argument('-s', '--sufix', help='specify a sufix for desired observation files', type=str, default=None)
+    P = p.parse_args()
+    
+    convertObs2HDF(folder = P.folder, sufix=P.sufix, odir=P.odir, override=P.force)
