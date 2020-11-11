bcc90a63cdde1a2264d984b3a34ed12e68a8c7cc
Michael Hirsch <scivision@users.noreply.github.com>
2020-11-11 16:43:44 -0500

    build hatanaka more robust

--- src/georinex/build.py
+++ src/georinex/build.py
@@ -5,15 +5,14 @@ import subprocess
 import shutil
 from pathlib import Path
 
-R = Path(__file__).parent / 'rnxcmp'
+R = Path(__file__).parent / "rnxcmp"
 
 
-def build(cc: str = None, src: Path = R / 'source/crx2rnx.c') -> int:
-    # C compiler search order from mesonbuild/environment.py
+def build(cc: str = None, src: Path = R / "source/crx2rnx.c") -> int:
     if cc:
         return do_compile(cc, src)
 
-    compilers = ['icl', 'cl', 'cc', 'gcc', 'clang', 'clang-cl', 'pgcc']
+    compilers = ["cc", "gcc", "clang", "icc", "icl", "cl", "clang-cl"]
     ret = 1
     for cc in compilers:
         if shutil.which(cc):
@@ -25,12 +24,15 @@ def build(cc: str = None, src: Path = R / 'source/crx2rnx.c') -> int:
 
 
 def do_compile(cc: str, src: Path) -> int:
-    if cc.endswith('cl'):  # msvc-like
-        cmd = [cc, str(src), '/Fe:'+str(R)]
-    else:
-        cmd = [cc, str(src), '-O2', '-o', str(R/'crx2rnx')]
+    if not src.is_file():
+        raise FileNotFoundError(src)
 
-    print(' '.join(cmd))
+    if cc.endswith("cl"):  # msvc-like
+        cmd = [cc, str(src), f"/Fe:{R}"]
+    else:
+        cmd = [cc, str(src), "-O2", f"-o{R / 'crx2rnx'}"]
+
+    print(" ".join(cmd))
     ret = subprocess.run(cmd).returncode
 
     return ret
